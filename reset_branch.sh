fbdde7c8849830a5a5206b20a2c1b8a8b94e2c3f
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 <14akshayrao@gmail.com>
2024-05-12 13:31:24 +0530

    [Rao] Auto merge/push in merge_into.sh and checks+default values for reset_branch.sh

---
+++ reset_branch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# reset_branch --from pre-release --to demo
+# reset_branch --from pre-release --to development
+
+#Read the argument values
+while [[ "$#" -gt 0 ]]
+  do
+    case $1 in
+      -f|--from) source_branch="$2"; shift;;
+      -r|--to) dest_branch="$2"; shift;;
+    esac
+    shift
+done
+
+source_branch="${source_branch:-pre-release}"
+
+if [[ ("$dest_branch" == "pre-release") || ("$dest_branch" == "main") ]]
+then
+  echo "Not allowed to reset '$dest_branch'"
+  exit 1
+fi
+
+echo "This will reset the '$dest_branch' branch using the '$source_branch' branch..."
+
+sleep 1
+echo ""
+echo "Press Ctrl+C within 5s to cancel..."
+
+sleep 5
+
+echo ""
+git checkout "$source_branch"
+git pull origin "$source_branch"
+git checkout "$dest_branch"
+git reset --hard "$source_branch"
+git push -f origin "$dest_branch"
