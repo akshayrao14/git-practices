a0ff6d5becd0bacbdd0abcefb657f14d23b2ec53
Akshay Rao <14akshayrao@gmail.com>
2024-10-12 19:46:05 +0530

    [Rao] Added a merge-into option for reset_branch.sh

--- scripts/reset_branch.sh
+++ scripts/reset_branch.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+source "$(dirname $0)/bash_formatting.sh"
 
 # reset_branch.sh TARGET         (assumes source to be pre-release)
 # OR
 # reset_branch.sh --to TARGET    (assumes source to be pre-release)
 # OR
 # reset_branch.sh --from SOURCE --to TARGET
+# OR
+# reset_branch.sh TARGET --merge-into (will merge your (current) feat branch after resetting
+#                                     before it pushes to origin)
 
-CUR_VERSION="1.0.3"
+CUR_VERSION="1.0.4"
 SCRIPT_NAME="reset_branch.sh"
 
 ####################################################################
@@ -28,7 +32,7 @@ else
   CUR_OSTYPE=$OSTYPE
 fi
 
-echo -e "${LOW_INTENSITY_TEXT}OS Detected: $CUR_OSTYPE"
+echo -e "${LOW_INTENSITY_TEXT_DIM}OS Detected: $CUR_OSTYPE"; RESET_FORMATTING
 ####################################################################
 
 #Read the argument values
@@ -40,6 +44,19 @@ while [[ "$#" -gt 0 ]]; do
     ;;
   -t | --to)
     dest_branch="$2"
+    shift
+    ;;
+  -m | --merge-into| --merge_into)
+    merge_feat_branch=$(git rev-parse --abbrev-ref HEAD)
+
+    disallowed_branches=("development" "demo" "develop" "main" "pre-release" "stag" "staging")
+    for disallowed_branch in "${disallowed_branches[@]}"; do
+      if [[ "$merge_feat_branch" == "$disallowed_branch" ]]; then
+        echo -e "${LIGHT_RED}Not allowed to use the merge-into option to merge '$merge_feat_branch' into '$dest_branch'."; RESET_FORMATTING
+        exit 1
+      fi
+    done
+
     shift
     ;;
   *) dest_branch="$1" ;;
@@ -56,16 +73,17 @@ fi
 
 echo "This will reset the '$dest_branch' branch using the '$source_branch' branch..."
 
-echo ""
+echo -e "${LOW_INTENSITY_TEXT_DIM}"
 (
   git checkout "$source_branch" &&
     git pull origin "$source_branch" &&
     (git branch -D "$dest_branch" &>/dev/null || true) &&
     git checkout "$dest_branch"
 ) || (
-  echo "something went wrong while switching branches!"
+  echo -e "${LIGHT_RED}something went wrong while switching branches!"
   exit 1
 )
+RESET_FORMATTING
 
 # if IS_MAC_OS is true, use -P in grep
 get_grep_pattern() {
@@ -103,27 +121,61 @@ if [[ "$merged_branches_since_last_reset" != "none" ]]; then
   fi
 fi
 
-echo "Press Ctrl+C within 5s to cancel..."
+echo "About to start. Press Ctrl+C within 5s to cancel..."
 
 sleep 5
 
 rm -rf delete.me
 
 echo "Resetting $dest_branch from $source_branch..."
+
+echo -e "${LOW_INTENSITY_TEXT_DIM}"
 git reset --hard "$source_branch"
 
 supTempFileAdd=$(touch delete.me && date >delete.me && git add delete.me)
 
 COMMIT_SUBJ="Resetting $dest_branch from $source_branch"
-COMMIT_LOST_DATA="Commits from these branches could be lost:
-...
+COMMIT_LOST_DATA="Commits from these branches could be lost: \
+... \
 ${merged_branches_since_last_reset:-none}"
 COMMIT_MSG="$COMMIT_SUBJ via reset_branch.sh (v$CUR_VERSION). $COMMIT_LOST_DATA"
 
-(git commit -m "$COMMIT_MSG" -n && git push origin $dest_branch --force) || (echo "something went wrong while pushing!" && exit 0)
+git commit -m "$COMMIT_MSG" -n && successOp=1 || successOp=0
+RESET_FORMATTING
 
-repoName="$(basename "$(git rev-parse --show-toplevel)")"
+if [[ "$successOp" == "0" ]]; then
+  echo -e "${LIGHT_RED}Failed to commit. Exiting..."; RESET_FORMATTING
+  exit 1
+fi
 
+if test -n "$merge_feat_branch"
+then
+  echo -e "${LIGHT_GREEN}Merging $merge_feat_branch into $dest_branch..."; RESET_FORMATTING
+  git merge "$merge_feat_branch" \
+    -m "AutoReset+Merge: Resetting $dest_branch from $source_branch and merge-into $merge_feat_branch via reset_branch.sh (v$CUR_VERSION)" && \
+    successOp=1 || successOp=0
+fi
+
+if [[ "$successOp" == "0" ]]; then
+  echo -e "${LIGHT_RED}Failed to merge. Try this without the --merge_into option. Exiting..."; RESET_FORMATTING
+  exit 1
+fi
+
+echo -e "${LIGHT_GREEN}Pushing $dest_branch to origin..."; RESET_FORMATTING
+echo -e "${LOW_INTENSITY_TEXT_DIM}"
+git push origin "$dest_branch" --force && successOp=1 || successOp=0
+RESET_FORMATTING
+
+if [[ "$successOp" == "0" ]]; then
+  echo -e "${LIGHT_RED}Failed to push. Exiting..."; RESET_FORMATTING
+  exit 1
+fi
+
+echo -e "${LIGHT_GREEN}Done! Sending out emails to let everyone know..."; RESET_FORMATTING
+
+###################################################################
+# Sending an email to alert people that the branch has been reset #
+###################################################################
 # fetches all the branches of the repo
 # delete any local branches whose remote counterpart is deleted
 # (helps in case we change the API key)
@@ -135,6 +187,7 @@ secretSuffix="/end"
 secretBranch=$(git branch -a | grep -m 1 $secretPrefix | head -1 | xargs)
 mgApiKey=$(echo "$secretBranch" | eval grep -o $(get_grep_pattern "$secretPrefix" "$secretSuffix") | xargs)
 
+repoName="$(basename "$(git rev-parse --show-toplevel)")"
 curGitUser=$(git config user.email)
 
 supMailer=$(curl -s --user "api:$mgApiKey" \
